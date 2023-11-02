21fe14d3987d31c6e66cf3890d3ed2ba531d5ace
Tonghun Hwang <tonghun.hwang@sportwiss.uni-hannover.de>
2023-11-02 22:53:50 +0100

    Update memo.py

--- pages/memo.py
+++ pages/memo.py
@@ -1,1 +1,37 @@
+import streamlit as st
+from datetime import datetime
+import os
 
+st.title("Memo")
+st.write("this is memo.")
+
+memo_list = ['newMemo'] + os.listdir('memo/')
+fn = st.selectbox('select memo.', memo_list)
+content = '' 
+
+if fn != 'newMemo':
+    with open(f'memo/{fn}', 'r', encoding='utf-8') as f:
+        content = f.read()
+else:
+    fn = datetime.now().strftime('%Y_%m_%d__%H_%M_%S') + '.txt'
+
+txt = st.text_area("put your memo.", value=content)
+
+left, right = st.columns(2)
+with left:
+    button = st.button ("save", use_container_width=True)
+with right:
+    down_button = st.download_button(
+        'download', data=txt, file_name=fn, use_container_width=True
+    )
+
+if button:
+    try:
+        st.write("button is clicked.")
+        st.write(txt)
+
+        with open(f'memo/{fn}', 'w', encoding='utf-8') as f:
+            f.write(txt)
+            st.success('memo is saved.')
+    except Exception as e:
+        st.error(e)
